d1a870a17e1a81ca54b6f62e66c9a38f3d59a04c
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-07-23 10:12:26 -0600

    Add files via upload

--- js/datatables-demo.js
+++ js/datatables-demo.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
       lengthMenu: "Filtro de _MENU_ registros",
       info: "Mostrando del registro _START_ al _END_ de un total de _TOTAL_ registros",
       infoEmpty: "No existen registros",
-      infoFiltered: "(filtrado de _MAX_ ventas en total)",
+      infoFiltered: "(filtro de _MAX_ registos en total)",
       infoPostFix: "",
       loadingRecords: "Cargando elementos...",
       zeroRecords: "No se encontraron los datos de tu busqueda..",
